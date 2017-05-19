7a130831a8ab2003bf4107b89a82e6aee06b8454
TheZubaz <schellekens.glenn@gmail.com>
2017-05-19 09:38:24 +0200

    alles abi
    homepage layout

--- views/layout.php
+++ views/layout.php
@@ -1,39 +1,59 @@
 <!DOCTYPE html>
 <html>
     <head>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <meta charset="UTF-8">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="<?= $GLOBALS['config']['base_url'] ?>assets/css/site.css">
         <script type="text/javascript" src="<?= $GLOBALS['config']['base_url'] ?>assets/js/script.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
         <link rel="shortcut icon" href="favicon.ico" />
 		<link rel="icon" href="<?= $GLOBALS['config']['base_url'] ?>assets/favicon.ico" type="image/x-icon" />
-        <title><?=$Cvar['page_title'] ?></title>
+        <title>UXXX</title>
+		
     </head>
     <body>
         <?php if (isset($_SESSION['user']['role']) && $_SESSION['user']['role'] == 777) : ?>
             <div id="admin">
-                <style> div#main-body { min-height: calc(100vh - 86px) !important; } </style>
                 <a class="float-left">Admin</a>
-                <a class="float-left" href="<?= $GLOBALS['config']['base_url'] ?>users/overview">Overview</a>
             </div>
         <?php endif; ?>
 
-        <header id="head">
-            <img class="float-left header-img" src="<?= $GLOBALS['config']['base_url'] ?>assets/rainbow-icon.png">
-            <span class="hamburg">Menu</span>
-            <a class="float-left" href="<?= $GLOBALS['config']['base_url'] ?>">Home</a>
-            <a class="float-left" href="<?= $GLOBALS['config']['base_url'] ?>pages/faq">FAQ</a>
+	<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img alt="Brand" class="header-img" src="<?= $GLOBALS['config']['base_url'] ?>assets/uxxx.png"></a>
+    </div>
 
-            <?php if (!isset($_SESSION['user']['id'])) : ?>
-                <a class="float-right" href="<?= $GLOBALS['config']['base_url'] ?>users/login">Login</a>
-                <a class="float-right" href="<?= $GLOBALS['config']['base_url'] ?>users/create">Register</a>
-            <?php else : ?>
-                <a class="float-right" href="<?= $GLOBALS['config']['base_url'] ?>users/logout">Logout(<?=$_SESSION['user']['name'] ?>)</a>
-                <a class="float-right" href="<?= $GLOBALS['config']['base_url'] ?>users/view">Account</a>
-            <?php endif; ?>
-        </header>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+					<li><a href="<?= $GLOBALS['config']['base_url'] ?>">Home</a></li>
+					<li><a href="<?= $GLOBALS['config']['base_url'] ?>pages/faq">FAQ</a></li>
+					<?php if (isset($_SESSION['user']['role']) && $_SESSION['user']['role'] > 1) : ?>
+						<li><a href="<?= $GLOBALS['config']['base_url'] ?>users/overview">Gebruikers</a></li>
+					<?php endif; ?>
+					<?php if (!isset($_SESSION['user']['_id'])) : ?>
+						<li><a href="<?= $GLOBALS['config']['base_url'] ?>users/login">Login</a></li>
+					<?php else : ?>
+						<li><a href="<?= $GLOBALS['config']['base_url'] ?>users/logout">Logout(<?=$_SESSION['user']['name'] ?>)</a></li>
+						<li><a href="<?= $GLOBALS['config']['base_url'] ?>users/view">Portfolio</a></li>
+					<?php endif; ?>
+      </ul>
+ 
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 
         <div id="main-body">
             <div class="inner-body">
@@ -42,7 +62,7 @@
         </div>
 
         <footer id="foot">
-            By Simon Striekwold.
+            By Simon Striekwold & Boyke.
         </footer>
     </body>
 <html>
--- views/pages/home.php
+++ views/pages/home.php
@@ -1,5 +1,14 @@
-<p>Hallo <?= $Cvar['name']; ?>!</p>
-<br>
-<p>De regenboog is een middelbare school waar meer dan 400 leerlingen bezig zijn met
-een vmbo, havo, of vwo-diploma.<br> De school is gevestigd op de Schoolstraat 1,
-Arnhem en is opgericht in 1970.</p>
+<div class="container-fluid">
+	<div class="row hidden-xs">
+		<div class="col-md-12">
+			<img class="banner-img .hidden-sm" src="assets/banner.png">
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h1 class="pop-prod">Populaire producten</h1>
+		</div>
+	</div>
+</div>
