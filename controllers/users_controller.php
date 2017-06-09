9220e6ff4a932ea2903dd82e93762bd505406728
a-lot-of-keks <simonplanet@hotmail.com>
2017-06-09 10:26:47 +0200

    pls fix

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -1,5 +1,6 @@
 <?php
-    require_once('./models/user.php');
+    require "models/user.php";
+    require "models/order.php";
 
     class usersController {
 
@@ -30,25 +31,61 @@
 
         public static function overview() {
             if (isset($_SESSION['user']['role']) && $_SESSION['user']['role'] == 777) {
-				if (isset($_POST['search'])) {
-					$search = base::Sanitize($_POST['search']);
-					$users = user::searchByName($search, 11, 0);
-					Base::Render('users/overview', [
-						'users' => $users
-					]);
-				} else {
-					$users = user::searchByName('', 11, 0);
-					Base::Render('users/overview', [
-						'users' => $users
-					]);
-				}
+				if (isset($_POST['var2']) && !empty($_POST['var2'])) {
+                    Base::Redirect($GLOBALS['config']['base_url'].'users/overview/1/'.Base::Sanitize($_POST['var2']));
+                } elseif (isset($_POST['var2']) && empty($_POST['var2'])) {
+                    Base::Redirect($GLOBALS['config']['base_url'].'users/overview/1');
+                }
+
+                if (isset($_GET['var1'])) {
+                    $page = (int) Base::Sanitize( $_GET['var1'] );
+                    if ($page < 1) {
+                        $url = Base::Curl();
+                        $url = str_replace('0', '1', $url);
+
+                        Base::Redirect( $url );
+                    }
+                } else {
+                    $page = 1;
+                }
+
+                if (isset($_GET['var2'])) {
+                    $search = base::Sanitize($_GET['var2']);
+                    $users = User::searchByName($search, 8, (($page - 1) * 8) );
+                } else {
+                    $users = User::searchByName('', 8, (($page - 1) * 8) );
+                }
+
+                if (isset($_GET['var2'])) {
+                    $searchpar = '/'.$_GET['var2'];
+                } else {
+                    $searchpar = null;
+                }
+
+                Base::Render('users/overview', [
+                    'users' => $users,
+                    'page' => $page,
+                    'searchpar' => $searchpar
+                ]);
             } else {
                 Base::Render('pages/error');
             }
         }
 
         public static function view() {
-            Base::Render('users/view');
+            $id = Base::Sanitize( $_GET['var1'] );
+            $user = User::Find($id);
+
+            if ($user !== false && (($user->id == $_SESSION['user']['id'] && $user->password == $_SESSION['user']['password']) || ($_SESSION['user']['role'] == 777))) {
+                $orders = Order::FindByUser($id);
+                
+                Base::Render('users/view', [
+                    'user' => $user,
+                    'orders' => $orders
+                ]);
+            } else {
+                Base::Render('pages/error');
+            }
         }
 
         public static function create() {
@@ -59,11 +96,18 @@
                 $_POST['user']['password'] === $_POST['user']['passwordrep'] &&
                 !user::findByName($_POST['user']['name'])
             ) {
+                if ( $_FILES['pic']['size'] > 0 ) {
+                    $pic = Base::Upload_file( $_FILES['pic'] );
+                } else {
+                    $pic = 'assets/img/user.png';
+                }
+                
                 $user = new User(
                     Base::Genetate_id(),
                     Base::Sanitize( $_POST['user']['name'] ),
                     Base::Hash_String( $_POST['user']['password'] ),
-                    1
+                    1,
+                    $pic
                 );
 
                 if ($user->save()) {
@@ -72,7 +116,8 @@
                             "id" => $user->id,
                             "name" => $user->name,
                             "password" => $user->password,
-                            "role" => $user->role
+                            "role" => $user->role,
+                            "pic" => $user->pic
                         ];
                     }
 
@@ -106,8 +151,21 @@
                         $user->password = Base::Hash_String($_POST['user']['password']);
                     }
 
+                    if ($_FILES['pic']['size'] > 0) {
+                        $user->pic = Base::Upload_file( $_FILES['pic'] );
+                    }
+
                     if ($user->save()) {
-                        Base::Redirect($GLOBALS['config']['base_url'] . "users/edit/" . $user->id);
+                        if ( $_SESSION['user']['id'] == $user->id ) {
+                            $_SESSION['user'] = [
+                                "id" => $user->id,
+                                "name" => $user->name,
+                                "password" => $user->password,
+                                "role" => $user->role,
+                                "pic" => $user->pic
+                            ];
+                        }
+                        Base::Redirect($GLOBALS['config']['base_url'] . "users/view/" . $user->id);
                     } else {
                         Base::Render('pages/error');
                     }
@@ -134,6 +192,22 @@
                 }
             } else {
                 Base::Render('pages/error');
+            }
+        }
+
+        public static function addusers() {
+            for ($i=1; $i < 20; $i++) {
+                $usr = new User(
+                    Base::Genetate_id(),
+                    Base::Sanitize('test'.$i ),
+                    Base::Hash_String('test'.$i),
+                    1,
+                    'assets/img/user.png'
+                );
+
+                if ( !$usr->save() ) {
+                    echo'error';exit;
+                }
             }
         }
     }
--- views/users/overview.php
+++ views/users/overview.php
@@ -1,34 +1,41 @@
 <div class="container portrait">
 	<div class="row">
-		<div class="col-lg-3">
+		<div class="col-md-3">
 			<form method="POST">
 				<div class="input-group search">
-					<input name="search" value="<?php if (isset ($_POST['search'])) { echo $_POST['search']; } ?>" type="text" class="form-control" placeholder="Search for...">
+					<input name="var2" value="<?php if (isset ($_POST['search'])) { echo $_POST['search']; } ?>" type="text" class="form-control" placeholder="Search for...">
 					<span class="input-group-btn">
 						<button class="btn btn-default" type="submit">Go!</button>
 					</span>
 				</div>
 			</form>
 		</div>
+		<div class="col-md-9">
+			<a href="<?= $GLOBALS['config']['base_url'] ?>users/create" class="btn btn-default pull-right">add user</a>
+		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-3 col-sm-6">
-			<div class="overview-item" style="background-image: url(<?=$GLOBALS['config']['base_url'].'assets/user.png' ?>);">
-				<a href="<?= $GLOBALS['config']['base_url'] ?>users/create" class="btn">add user</a>
+		<hr class="hr-mar">
+	</div>
+	<div class="row">
+		<?php foreach ($users as $key => $user) : ?>
+			<div class="col-md-5 <?php if (!(floor($key/2) == ($key/2))) : ?>col-md-offset-2<?php endif; ?> user-list-item">
+				<a class="view" href="<?=$GLOBALS['config']['base_url'].'users/view/'.$user['id'] ?>">
+					<div style="background-image: url(<?=$GLOBALS['config']['base_url'].$user['pic'] ?>);" class="img"></div>
+					<span class="name"><?=$user['name'] ?></span>
+					<span class="role"><?=user::role($user['role']) ?></span>
+				</a>
+				<a class="edit" href="<?= $GLOBALS['config']['base_url'] ?>users/edit/<?=$user['id'] ?>" class="btn">edit</a>
+				<a class="del" href="<?= $GLOBALS['config']['base_url'] ?>users/delete/<?=$user['id'] ?>" class="del">x</a>
+				<hr>
 			</div>
+		<?php endforeach; ?>
+	</div>
+	<div class="row">
+		<div class="col-md-2 col-md-offset-5">
+			<a href="<?=$GLOBALS['config']['base_url'].'users/overview/'.($page-1).$searchpar ?>" class="<?php if ($page == 1) : ?>disabled<?php endif; ?>">previous</a>
+			<a href="<?=$GLOBALS['config']['base_url'].'users/overview/'.($page+1).$searchpar ?>" class="pull-right">next</a>
 		</div>
-			<?php foreach ($users as $user) : 
-				if ($user['id'] != $_SESSION['user']['id']) : ?>
-				<div class="col-md-3 col-sm-6">
-					<div class="overview-item" style="background-image: url(<?=$GLOBALS['config']['base_url'].'assets/noPicture.png' ?>);">
-						<span><?=$user['name'] ?></span>
-						<span><?=user::role($user['role']) ?></span>
-						<a href="<?= $GLOBALS['config']['base_url'] ?>users/edit/<?=$user['id'] ?>" class="btn">edit</a>
-						<a href="<?= $GLOBALS['config']['base_url'] ?>users/delete/<?=$user['id'] ?>" class="del">x</a>
-					</div>
-				</div>
-			<?php endif;
-			 endforeach; ?>
 	</div>
 </div>
 			
