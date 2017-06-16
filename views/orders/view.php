ea27868de42d57df91f6a6ea49dd46956d42356c
a-lot-of-keks <simonplanet@hotmail.com>
2017-06-16 09:43:01 +0200

    search change

--- views/orders/view.php
+++ views/orders/view.php
@@ -1,12 +1,12 @@
 <div class="container">
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-md-4 col-md-offset-4">
             <br>
-            <p><?= $order->method ?></p>
-            <p><?= $order->status ?></p>
-            <p><?= $order->paidDatetime ?></p>
-            <p><?= $order->details_consumerName ?></p>
-            <p><?= $order->details_consumerAccount ?></p>
+            <p><b>Payment method:</b> <?= $order->method ?></p>
+            <p><b>Payment status:</b> <?= $order->status ?></p>
+            <p><b>Payment method:</b> <?= $order->paidDatetime ?></p>
+            <p><b>Payment method:</b> <?= $order->details_consumerName ?></p>
+            <p><b>Payment method:</b> <?= $order->details_consumerAccount ?></p>
         </div>
     </div>
 </div>
