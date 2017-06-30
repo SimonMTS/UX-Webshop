ada5e819287060027e5a0df5cfe89bee9e9c3cad
TheZubaz <schellekens.glenn@gmail.com>
2017-06-30 16:34:08 +0200

    orders overview styling

--- views/orders/overview.php
+++ views/orders/overview.php
@@ -11,15 +11,33 @@
             </form>
         </div>
     </div>
-	<div class="row">
-		<?php foreach ($orders as $order) : ?>
-            <?php echo $order['game_name']; ?><br>
-		<?php endforeach; ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 order-list <?php if (sizeof($orders) == 0) : ?>hidden-sm hidden-xs<?php endif; ?>">
+				<div style="display: none;" class="">
+					<?php if (sizeof($orders) == 0) : ?>
+						<span class="no-p">Nog geen aankopen</span>
+					<?php endif; ?>
+					<?php foreach ($orders as $order) : ?>
+						<div>
+							<a class="left" href="<?=$GLOBALS['config']['base_url'].'orders/view/'.$order['id'] ?>">
+								<span class="full-w-left"><?= $order['method'] ?> - See full order</span>
+								<span class="paid"><?= $order['status'] ?></span>
+								</a>
+								<a class="right" href="<?=$GLOBALS['config']['base_url'].'orders/overview/'.$order['game_id'] ?>">
+								<span class="pull-right"><?= date('d/m/Y', strtotime($order['paidDatetime']) ) ?></span><br>
+								<span class="pull-right full-w"><?= $order['game_name'] ?></span>
+							</a>
+						</div>
+					<?php endforeach; ?>
+				</div>
+			</div>
+		</div?
 	</div>
 	<div class="row">
 		<div class="col-md-2 col-md-offset-5">
-			<a href="< ?=$GLOBALS['config']['base_url'].'games/overview/'.($page-1).$searchpar ?>" class="< ?php if ($page == 1) : ?>disabled< ?php endif; ?>">previous</a>
-			<a href="< ?=$GLOBALS['config']['base_url'].'games/overview/'.($page+1).$searchpar ?>" class="pull-right">next</a>
+			<a href="< ?=$GLOBALS['config']['base_url'].'orders/overview/'.($page-1).$searchpar ?>" class="< ?php if ($page == 1) : ?>disabled< ?php endif; ?>">previous</a>
+			<a href="< ?=$GLOBALS['config']['base_url'].'orders/overview/'.($page+1).$searchpar ?>" class="pull-right">next</a>
 		</div>
 		<div class="col-md-12">
 			<br>
